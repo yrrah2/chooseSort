0786874648f0ccde106e4492182829c6ca408433
Harry Hackett <yrrah2@users.noreply.github.com>
2021-01-26 20:31:38 +1300

    Added close tab warning after sorting starts

--- images/script.js
+++ images/script.js
@@ -1,4 +1,5 @@
 const readURL = (input) => {
+	imagesLoaded = true;
 	let files = input.files;
 	let urls = [];
 	sortingArray = [];
@@ -35,7 +36,22 @@ const showOptions = (left, right) => {
 	$("#right").attr("src", right);
 }
 
+const pageunload = (e) => {
+	if (imagesLoaded) {
+		e = e || window.event;
+
+		// For IE and Firefox prior to version 4
+		if (e) {
+			e.returnValue = 'Sure?';
+		}
+
+		// For Safari
+		return 'Sure?';
+	}
+}
+
 const pageload = () => {
+	imagesLoaded = false;
 	sortingArray = ["https://i.imgur.com/imDCn1v.jpg", "https://i.imgur.com/2x4n776.jpg", "https://i.imgur.com/8PsLHJ7.jpg", "https://i.imgur.com/6hB0JIO.jpg", "https://i.imgur.com/vbeK6YW.jpg", "https://i.imgur.com/4sXCzxQ.jpg", "https://i.imgur.com/8RFpSAj.jpg", "https://i.imgur.com/B14Epbk.jpg", "https://i.imgur.com/RFXCmAx.jpg", "https://i.imgur.com/98VEgnZ.jpg", "https://i.imgur.com/e89hrJx.jpg", "https://i.imgur.com/ZDOLJzy.jpg", "https://i.imgur.com/GefZLkJ.jpg", "https://i.imgur.com/vwbn7Uk.jpg", "https://i.imgur.com/1JdwKPb.jpg", "https://i.imgur.com/dShJR4z.jpg", "https://i.imgur.com/nHsabjm.jpg"];
 	sortingComplete = false;
 	nextStage();
@@ -45,3 +61,4 @@ const pageload = () => {
 };
 
 window.onload = pageload;
+window.onbeforeunload = pageunload;
